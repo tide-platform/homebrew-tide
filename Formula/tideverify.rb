db4be8375f56d07311531835a04a5b6dd9807ac1
tide-platform <angkan@evillogic.net>
2025-07-31 10:31:49 +0000

    Update tideverify.rb

--- Formula/tideverify.rb
+++ Formula/tideverify.rb
@@ -2,7 +2,7 @@ class Tideverify < Formula
     desc "Tide platform - Out of Band Verification"
     homepage "https://github.com/tide-platform/"
     url "https://github.com/tide-platform/homebrew-tide/archive/refs/tags/tideverifyv1.0.tar.gz"
-    sha256 "31546754b63cd41eb8b370b0c3358daaf5305cdfc8cb4c122d2ac4f7fb43fe6a"
+    sha256 "9f92f6217aed6b9c72be92a08bad278e70dfcefa930c8def74484c115eec92cd"
     version "1.0"
 
     def install
