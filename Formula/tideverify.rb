705ed83e24a708ffd65716cc5ebe0513ce45cc1b
Mikail Tunc <angkan@evillogic.net>
2025-07-31 10:24:08 +0000

    Initial brew tap commit

--- Formula/tideverify.rb
+++ Formula/tideverify.rb
@@ -1,8 +1,8 @@
 class Tideverify < Formula
     desc "Tide platform - Out of Band Verification"
     homepage "https://github.com/tide-platform/"
-    url "https://github.com/tide-platform/homebrew-tideverify/releases/download/v1.0/tideverify-1.0.tar.gz"
-    sha256 "921f2a29a2aa346e93607b524817ea0227b1e636d2c3177c7c02b408f95b944a"
+    url "https://github.com/tide-platform/homebrew-tide/releases/download/v1.0/tideverify-1.0.tar.gz"
+    sha256 "31546754b63cd41eb8b370b0c3358daaf5305cdfc8cb4c122d2ac4f7fb43fe6a"
     version "1.0"
 
     def install
